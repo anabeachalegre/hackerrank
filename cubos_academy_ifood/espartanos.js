e9ede9870fde6ccdb191c508d322367c58fefbb2
Ana Beatriz Chalegre <chalegreana@gmail.com>
2022-08-01 15:31:06 -0300

    first commit

---
+++ cubos_academy_ifood/espartanos.js
@@ -0,0 +1,11 @@
+//link do desafio: https://www.hackerrank.com/contests/psel-ifood-b2bt03/challenges/espartanos
+
+//resolução
+
+function solucao(inimigo, espartano) {
+	if (inimigo > (espartano*4)) {
+        console.log("INIMIGO"); 
+    } else { 
+        console.log("ESPARTANOS")
+    }
+}
